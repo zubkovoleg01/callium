602c4817469e4b4ae516a48ca7d94a14ce81fe4a
zubkovoleg01 <120819704+zubkovoleg01@users.noreply.github.com>
2023-01-15 20:43:21 +0300

    The main structure of the project.

---
+++ js/lib/slider-main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function mainSlider () {
+  let mySwiper = '';
+  let breakpoint = window.matchMedia('(max-width: 767px)');
+  let breakpointChecker = function () {
+    if (breakpoint.matches) {
+      if (mySwiper) {
+        mySwiper.destroy(true, true);
+      }
+
+      mySwiper = new Swiper('.main-screen__slider', {
+        spaceBetween: 10,
+        pagination: {
+          el: '.main-screen__slider-pagination',
+          type: 'fraction',
+        },
+
+        scrollbar: {
+          el: '.main-screen__slider-scrollbar',
+        },
+      });
+      return;
+    } else {
+      if (mySwiper) {
+        mySwiper.destroy(true, true);
+      }
+
+      mySwiper = new Swiper('.main-screen__slider', {
+        pagination: {
+          el: '.main-screen__slider-pagination',
+          type: 'bullets',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + '0' + (index + 1) + '</span>';
+          },
+        },
+      });
+    }
+  };
+  breakpoint.addListener(breakpointChecker);
+  breakpointChecker();
+};
+
+mainSlider();
